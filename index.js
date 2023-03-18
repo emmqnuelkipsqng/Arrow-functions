d1d2a905a6aa20d44f59dab8d12e36206b9b4f59
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-03-17 22:26:09 -0700

    completed asignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// Define the divide function
+const divide = () => {
+    return 2000 / 100;
+  };
+  
+  // Define the square function
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Define the add function
+  const add = (a, b) => {
+    return a + b;
+  };
